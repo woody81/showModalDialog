4b171e064725621fa68c299ed0ce228ae6fe94e6
lesakon <ondrej.lesak@storaenso.com>
2019-03-11 13:14:22 +0100

    vyjmuti tlacitka pro uzavreni modalniho okna mimo iframe refactoring

--- js/showModalDialog-poly.js
+++ js/showModalDialog-poly.js
@@ -1,47 +1,17 @@
 (function() {
-    window.spawn = window.spawn || function(gen) {
-        function continuer(verb, arg) {
-            var result;
-            try {
-                result = generator[verb](arg);
-            } catch (err) {
-                return Promise.reject(err);
-            }
-            if (result.done) {
-                return result.value;
-            } else {
-                return Promise.resolve(result.value).then(onFulfilled, onRejected);
-            }
-        }
-        var generator = gen();
-        var onFulfilled = continuer.bind(continuer, 'next');
-        var onRejected = continuer.bind(continuer, 'throw');
-        return onFulfilled();
-    };
     window.showModalDialog = window.showModalDialog || function(url, arg, opt) {
         url = url || ''; //URL of a dialog
         arg = arg || null; //arguments to a dialog
         opt = opt || 'dialogWidth:300px;dialogHeight:200px'; //options: dialogTop;dialogLeft;dialogWidth;dialogHeight or CSS styles
-        var caller = showModalDialog.caller.toString();
-        /*var dialog = document.body.appendChild(document.createElement('dialog'));
-        dialog.setAttribute('style', opt.replace(/dialog/gi, ''));
-        dialog.innerHTML = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 5px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a><iframe id="dialog-body" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
-        document.getElementById('dialog-body').contentWindow.dialogArguments = arg;
-        document.getElementById('dialog-close').addEventListener('click', function(e) {
-            e.preventDefault();
-            dialog.close();
-        });
-        dialog.showModal();*/
 
-        var cover = $('<div>').addClass('cover').appendTo(document.body);
         var close = function () {
             iframe.remove();
             content.remove();
             cover.remove();
         };
-        var content = $('<div>')
-        .addClass('dialog')
-        .css({
+
+        var cover = $('<div>').addClass('cover').appendTo(document.body);
+        var content = $('<div>').addClass('dialog').css({
             width: opt.width || '50%',
             height: opt.height || '50%',
             left: opt.left || '25%',
@@ -50,39 +20,31 @@
         })
         .appendTo(document.body);
 
-        var iframe = $('<iframe>').appendTo(content).attr("src", url)
-        .on("load", function () {
+        var iframe = $('<iframe>').appendTo(content).attr({
+                "src": url,
+                "id": "modal-iframe"}).on("load", function () {
             content.css({ display: 'block' });
         });
 
-        //if using yield or async/await
-        if(caller.indexOf('yield') >= 0 || caller.indexOf('await') >= 0) {
-            promise().done(function() {
-                return new Promise(function(resolve, reject) {
-                    var cancelButton = content.getElementById('cancel-button');
-                    cancelButton.addEventListener('close', function() {
-                        var returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
-                        // document.body.removeChild(dialog);
-                        close();
-                        resolve(returnValue);
-                    });
-                });
-            })
+        $('<input>').appendTo(content).attr({
+            "type": "button",
+            "id": "dialog-close",
+            "value": "close"});
 
-
-        }
-        //if using eval
-        var isNext = false;
-        var nextStmts = caller.split('\n').filter(function(stmt) {
-            if(isNext || stmt.indexOf('showModalDialog(') >= 0)
-                return isNext = true;
-            return false;
+        $('#dialog-close').on('click',function(e) {
+            e.preventDefault();
         });
-        document.getElementById('cancel-button').addEventListener('close', function() {
-            var returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
-            document.body.removeChild(dialog);
-            nextStmts[0] = nextStmts[0].replace(/(window\.)?showModalDialog\(.*\)/g, JSON.stringify(returnValue));
-            eval('{\n' + nextStmts.join('\n'));
+
+        return new Promise(function(resolve, reject) {
+            // document.getElementsByTagName('input')[0].addEventListener('click', function() {
+            $('#dialog-close').on('click', function () {
+                // $(this).contents().find("input").on("close", function() {
+                // var returnValue = document.getElementById('modal-iframe').contentWindow.returnValue;
+                var returnValue = document.getElementById('modal-iframe').contentWindow.returnValue;
+                close();
+                // document.body.removeChild(dialog);
+                resolve(returnValue);
+            });
         });
         throw 'Execution stopped until showModalDialog is closed';
     };
