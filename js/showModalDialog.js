c30b837f3926a6ee5ea02b4950fc3aa88b8c7686
lesakon <ondrej.lesak@storaenso.com>
2019-03-12 18:24:46 +0100

    pridan async use case

--- js/showModalDialog.js
+++ js/showModalDialog.js
@@ -1,36 +1,42 @@
-async function showModalDialog(url, args, options) {
-    options = options || {};
-    return new Promise(function (fullfil, fail) {
-		var cover = $('<div>')
-			.addClass('cover')
-			.appendTo(document.body);
-		var close = function () {
-				iframe.remove();
-				content.remove();
-				cover.remove();
-			};
-		var content = $('<div>')
-			.addClass('dialog')
-			.css({
-				width: options.width || '50%',
-				height: options.height || '50%',
-				left: options.left || '25%',
-				top: options.top || '25%',
-				display: 'none'
-			})
-			.appendTo(document.body);
-		var iframe = $('<iframe>').appendTo(content).attr("src", url)
-			.on("load", function () {
-				content.css({ display: 'block' });
-			});
-		showModalDialog.dialogArguments = args;
-		showModalDialog.returnValue = function (v) {
-			close();
-			fullfil(v);
-		};
-		showModalDialog.cancel = function () {
-			close();
-			fail();
-		};
+(function() {
+  window.showModalDialog = window.showModalDialog || function(url, arg, opt) {
+    url = url || ''; //URL of a dialog
+    arg = arg || null; //arguments to a dialog
+    opt = opt || 'dialogWidth:300px;dialogHeight:200px'; //options: dialogTop;dialogLeft;dialogWidth;dialogHeight or CSS styles
+
+    var close = function () {
+      iframe.remove();
+      content.remove();
+      cover.remove();
+    };
+
+    var cover = $('<div>').addClass('cover').appendTo(document.body);
+    var content = $('<div>').addClass('dialog').css({
+      width: opt.width || '50%',
+      height: opt.height || '50%',
+      left: opt.left || '25%',
+      top: opt.top || '25%',
+      display: 'none'
+    })
+    .appendTo(document.body);
+
+    var iframe = $('<iframe>').appendTo(content).attr({
+      "src": url,
+      "id": "modal-iframe"}).on("load", function () {
+      content.css({ display: 'block' });
     });
-}
+
+    return new Promise(function (resolve, reject) {
+      // document.getElementsByTagName('input')[0].addEventListener('click', function() {
+      $('#modal-iframe').on('load', function () {
+        $('#modal-iframe').contents().find("#close-link").on('click', function () {
+        // $(this).contents().find("input").on("close", function() {
+        var returnValue = document.getElementById('modal-iframe').contentWindow.returnValue;
+        close();
+        resolve(returnValue);
+      });
+      });
+    });
+    throw 'Execution stopped until showModalDialog is closed';
+  };
+  })();
